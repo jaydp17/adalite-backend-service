a3e08dcb3ce416b200204fc35c923d5106cc8b4c
Jaydeep Solanki <jaydp17@gmail.com>
2020-06-29 22:31:41 +0530

    add console.log

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ require('dotenv').config()
 // eslint-disable-next-line import/no-commonjs
 const config = require('config')
 
+console.log('config', JSON.stringify(config, null, 2))
+
 export default config
